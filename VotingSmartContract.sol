82b6eb705edec6ab7a08ab8c9cc4e4170790ce83
pankaj443 <pankajofficial443@gmail.com>
2019-08-11 00:46:14 +0530

    voting completet

--- VotingSmartContract.sol
+++ VotingSmartContract.sol
@@ -12,6 +12,8 @@ contract Voting{
     
     uint public WinnerVotes;
     
+    enum VotingStatus {NotStarted, Running, Completed}
+    VotingStatus public status;
     
     constructor() public{
         
@@ -26,12 +28,52 @@ contract Voting{
         }
     }
     
+    function SetStatus() public{
+        
+        if(status != VotingStatus.Completed){
+            status = VotingStatus.Running;
+        }else{
+            status = VotingStatus.Completed;
+        }
+        
+    }
+    
     function RegisteCandidate(address _candidate) OnlyOwner public{
         CandidateList.push(_candidate);
     }
     
     function Vote (address _candidate) public{
+        require(Validate(_candidate),"INVALID CANDIDATE");
+         require(status == VotingStatus.Running,"INVALID STATUS");
         VotesReceived[_candidate] += 1;
     }
+    
+   function Validate(address _candidate) public view returns(bool){
+       for(uint  i =0;i< CandidateList.length ; i++){
+           if(CandidateList[i] == _candidate){
+               return true;
+           }
+       }
+       return false;
+   }
+   
+   function VoteCount(address _candidate) public view returns(uint){
+         require(Validate(_candidate),"INVALID CANDIDATE");
+         return VotesReceived[_candidate];
+   }
+   function Result() public{
+       
+       require(status == VotingStatus.Completed,"INVALID STATUS");
+       
+       for( uint i =0 ;i<CandidateList.length ;i++){
+           if(VotesReceived [CandidateList[i]] > WinnerVotes){
+               WinnerVotes = VotesReceived[CandidateList[i]];
+               winner = CandidateList[i];
+           }
+       }
+       
+   }
+   
+   
      
 }
